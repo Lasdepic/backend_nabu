49129253c3b07090702f99a1df99a426fe02dc97
Clément Jordan <lasdepic@MacBook-Air-de-Clement.local>
2026-01-09 13:48:27 +0100

    first commit

---
+++ Config/Database.php
@@ -0,0 +1,29 @@
+<?php
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+class Database
+{
+    private static ?PDO $pdo = null;
+
+    public static function getConnexion(): ?PDO
+    {
+        if (self::$pdo === null) {
+            try {
+                $host = $_ENV['DB_HOST'];
+                $dbname = $_ENV['DB_NAME'];
+                $user = $_ENV['DB_USER'];
+                $pass = $_ENV['DB_PASS'];
+
+                self::$pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+                self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                echo 'Erreur de connexion avec la base de donnée : ' . $e->getMessage();
+                return null;
+            }
+        }
+        return self::$pdo;
+    }
+}
